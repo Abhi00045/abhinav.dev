8ba8dba16b1e848fe30c482f5909a399701c08b9
abhi <rajidiabhinavreddy@gmail.com>
2025-08-12 02:40:58 +0530

    added batman

--- src/Components/resume.jsx
+++ src/Components/resume.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import '../Components/resume.css'
 
 const ResumeCTA = () => {
-  const [likes, setLikes] = useState(2);
+  // const [likes, setLikes] = useState(2);
 
-  const handleLike = () => {
-    setLikes(likes + 1);
-  };
+  // const handleLike = () => {
+  //   setLikes(likes + 1);
+  // };
 
   return (
     <div className="resume-cta-container">
@@ -15,11 +15,12 @@ const ResumeCTA = () => {
         <p className="sub-line">maybe you’ll also like it</p>
       </div>
 
-      <div className="like-button" onClick={handleLike}>
+      {/* <div className="like-button" onClick={handleLike}>
         <span role="img" aria-label="heart">🤍</span> {likes}
-      </div>
+      </div> */}
 
-      <div className="footer">
+      <div className="footer flex justify-center" >
+        <img src="https://64.media.tumblr.com/9f71811b188248bd7fb217a9e915b566/tumblr_n3p1a3E6V31szb9vao1_1280.gif" className='w-60' alt="" />
         <pre>Built with 🤍 &lt;/&gt; ☕</pre>
       </div>
     </div>
