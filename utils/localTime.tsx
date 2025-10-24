8be621381ef3296b7401583c9f81134d8575f955
abhi <rajidiabhinavreddy@gmail.com>
2025-10-24 16:45:04 +0530

    timer update

---
+++ utils/localTime.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export const useLocalTime = () => {
+
+    const[Time , setLocalTime]=useState(new Date())
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setLocalTime(new Date())
+        },1000)
+        return () => clearInterval(interval)    
+    },[])
+
+
+    return(
+        <div>
+            {Time.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', second: '2-digit'})}
+        </div>
+    )
+}
