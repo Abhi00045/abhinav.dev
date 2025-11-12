536073b3cdd521312d0c06ecb676ef14fb099220
abhi <rajidiabhinavreddy@gmail.com>
2025-11-13 00:05:35 +0530

    col

--- utils/player.tsx
+++ utils/player.tsx
@@ -149,7 +149,7 @@ const Player: React.FC = () => {
       {/* Album Art */}
       <div className="aspect-square rounded-2xl overflow-hidden flex items-center justify-center shadow-[0_18px_32px_-24px_rgba(15,23,42,0.8)]">
         <img
-          src="https://cdn.gulte.com/wp-content/uploads/2025/11/globe-trotter-first-song.jpg"
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/The_Weeknd_-_Hurry_Up_Tomorrow.png/250px-The_Weeknd_-_Hurry_Up_Tomorrow.png"
           alt="Globe Trotter art"
           draggable={false}
           className="w-full h-full object-cover rounded-2xl saturate-105 pointer-events-none m-2"
@@ -160,9 +160,9 @@ const Player: React.FC = () => {
       <section className="flex flex-col w-full gap-1">
         <div className="flex items-center justify-between w-full gap-4">
           <div className="min-w-0 flex-1">
-            <h3 className="text-sm font-semibold truncate">Globe Trotter</h3>
+            <h3 className="text-sm font-semibold truncate">The Weekend</h3>
             <p className="text-xs text-gray-400 truncate">
-              SSMB29 x GlobeTrotter
+              Timeless x Playboi
             </p>
           </div>
 
