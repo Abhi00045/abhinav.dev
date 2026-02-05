a9facfd5a5d629c18ef48b28223b31917deb2222
abhi <rajidiabhinavreddy@gmail.com>
2026-02-05 15:42:03 +0530

    pushed new favicon in title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -16,21 +15,29 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "portfolio.me",
   description: "Full stack developer portfolio",
+
+  // 👉 ADD YOUR IMAGE URL HERE
+  icons: {
+    icon: "https://i.pinimg.com/1200x/57/3d/37/573d37c027e9c74a90baf6f38c2b5a9f.jpg", // paste your image link here
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
-        <script defer src="https://cloud.umami.is/script.js" data-website-id="1c44c067-a3fa-43fd-8760-67e56669e164"></script>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="1c44c067-a3fa-43fd-8760-67e56669e164"
+        />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
